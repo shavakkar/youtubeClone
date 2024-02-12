81fea489ec6e708cc1ad9fe2942f458c5d897f66
shavakkar <shavakkar@gmail.com>
2024-02-12 21:04:39 +0530

    videocard on process

--- resources/js/Components/VideoCard.vue
+++ resources/js/Components/VideoCard.vue
@@ -20,6 +20,7 @@ let video = ref(null);
 watch(() => {
     show.value,
         (show) => {
+            video.value.play();
             if (show) {
                 showVideo.value = true;
                 video.value.play();
@@ -35,6 +36,65 @@ watch(() => {
 
 <template>
     <div class="relative">
-        <div class="rounded-lg bg-black m-2"></div>
+        <div
+            class="rounded-lg bg-black m-2"
+            :class="[
+                show
+                    ? 'absolute z-30 transition ease-in-out delay-150 hover:translate-y-8 hover:scale-125 hover:bg-[#202020] duration-300'
+                    : '',
+            ]"
+        >
+            <div
+                @mouseover="show = true"
+                @mouseleave="
+                    show = false;
+                    showVideo = false;
+                "
+            >
+                <img
+                    class="aspect-video cursor-pointer"
+                    :src="thumbnail || ''"
+                    :class="
+                        (show
+                            ? 'transition ease-in-out delay-150 rounded-l-lg'
+                            : 'rounded-lg',
+                        showVideo ? 'delay-350 hidden' : '')
+                    "
+                />
+                <div
+                    class="w-full h-full aspect-video cursor-pointer"
+                    :class="showVideo ? '' : 'delay-350 hidden'"
+                >
+                    <video ref="video" :src="videoUrl || ''" type="video/mp4" />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex mt-1.5">
+                    <div>
+                        <img
+                            :src="image || ''"
+                            class="rounded-full m-1.5 mt-2 flex items-baseline w-8 h-8"
+                        />
+                    </div>
+                    <div class="px-1.5 text-white mt-1">
+                        <div
+                            class="text-[17px] font-extrabold w-full cursor-pointer"
+                        >
+                            {{ title.substring(0, 100) }}
+                        </div>
+                        <div
+                            class="text-[14px] text-gray-300 font-extrabold flex gap-1 items-center cursor-pointer"
+                        >
+                            {{ user.substring(0, 30) }}
+                            <CheckCircle fillColor="#888888" :size="17" />
+                        </div>
+                        <div class="text-sm mb-1 text-gray-300 cursor-pointer">
+                            {{ views }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,26 +2,29 @@
 import { Head, Link } from "@inertiajs/vue3";
 
 import NavLayout from "@/Layouts/NavLayout.vue";
+import VideoCard from "@/Components/VideoCard.vue";
 
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
+// defineProps({
+//     canLogin: {
+//         type: Boolean,
+//     },
+// });
 </script>
 
 <template>
     <Head title="Youtube" />
-    <NavLayout> </NavLayout>
+    <NavLayout>
+        <div
+            class="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2"
+        >
+            <VideoCard
+                title="Cool city view"
+                user="test"
+                views="23k views - 3 days ago"
+                image="https://picsum.photos/id/230/100"
+                videoUrl="/videos/seoul.mp4"
+                thumbnail="/videos/Thumbnails/seoul.png"
+            />
+        </div>
+    </NavLayout>
 </template>
