26bc6c04cc7f1ad787c9e58f22a70fabec6f69d1
shavakkar <shavakkar@gmail.com>
2024-02-12 19:18:37 +0530

    on making Nav & sideNav

---
+++ resources/js/Components/VideoCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps, toRefs, ref, watch, onMounted } from "vue";
+import CheckCircle from "vue-material-design-icons/CheckCircle.vue";
+
+const props = defineProps({
+    title: String,
+    user: String,
+    views: String,
+    image: String,
+    videoUrl: String,
+    thumbnail: String,
+});
+
+const { title, user, views, image, videoUrl, thumbnail } = toRefs(props);
+
+let show = ref(false);
+let showVideo = ref(false);
+let video = ref(null);
+
+watch(() => {
+    show.value,
+        (show) => {
+            if (show) {
+                showVideo.value = true;
+                video.value.play();
+                return "";
+            }
+
+            showVideo.value = false;
+            video.value.pause();
+            video.value.currentTime = 0;
+        };
+});
+</script>
+
+<template>
+    <div class="relative">
+        <div class="rounded-lg bg-black m-2"></div>
+    </div>
+</template>
