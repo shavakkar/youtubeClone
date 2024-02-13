2c019687a212aa8d420b7b02a84bd547c1c12073
shavakkar <shavakkar@gmail.com>
2024-02-13 15:00:15 +0530

    added options

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Welcome')->name('home');
 });
+Route::get('/add-video', function () {
+    return Inertia::render('AddVideo');
+})->name('addVideo');
+Route::get('/delete-video', function () {
+    return Inertia::render('DeleteVideo');
+})->name('deleteVideo');
 
 Route::get('/videos/{id}', [VideosController::class, 'show'])->name('videos.show');
 
